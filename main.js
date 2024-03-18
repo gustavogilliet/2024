879ce8889caa541513a87c89cc4f332af80097b7
gustavogilliet <102318597+gustavogilliet@users.noreply.github.com>
2024-03-18 08:31:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const botoes=document.querySelectorAll(botao) ;
+const textos = document.querySelectorAll(“.aba-conteudo”);
+console.log(botoes)
+for (let i=0; i < botões.length; i ++) { console.log(i); }
+for(let i=0;i <botoes.length;i++)
+const contadores = document.querySelectorAll(“.contador”);
 
+    botoes[i].onclick= function() {
+    botoes[i].classList.add(ativo);
+    textos[i].classList.add(“ativo”); 
+    }
+    botoes[j].classList. remove(“ativo”);
+    textos[j].classList.remove(“ativo”); 
+
+    textos[j].classList.remove(“ativo”); 
+    textos[i].classList.add(“ativo”); 
+    const textos = document.querySelectorAll(“.aba-conteudo”);
+    const contadores = document.querySelectorAll(“.contador”);  const tempoObjetivo1 = new Date(“2024-10-05”);
